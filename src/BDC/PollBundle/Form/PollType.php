72d0e846ec712d7978891c462e0cad80fc8e0a88
nestor91sanchez <sanchez91nestor@gmail.com>
2017-03-20 12:45:08 -0400

    modified

--- src/BDC/PollBundle/Form/PollType.php
+++ src/BDC/PollBundle/Form/PollType.php
@@ -21,8 +21,8 @@ class PollType extends AbstractType
             ->add('name')
             ->add('status', 'choice', ['choices' =>['active'=>'Activa', 'inactive'=>'Inactiva', 'ended' => 'Finalizada']])
             ->add('id_user', 'hidden')
-            ->add('image_header', 'file', array('label' => 'Imagen cabecera', 'required' => true))
-            ->add('image_footer', 'file', array('label' => 'Imagen pie de pagina', 'required' => true));
+            ->add('image_header', 'file', array('label' => 'Imagen cabecera'))
+            ->add('image_footer', 'file', array('label' => 'Imagen pie de pagina'));
 
     }
     
