d4208a3d2c41c80413cc3a741aaa20e88a756da6
mauro <mauro@icox.com.ar>
2017-05-02 12:34:40 -0300

    update envio de email

--- src/BDC/PollBundle/Controller/EmailController.php
+++ src/BDC/PollBundle/Controller/EmailController.php
@@ -21,6 +21,7 @@ class EmailController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $error = Null;
         $utils = new BDCUtils;
         
         if ($utils->check_session() === null) {
@@ -37,65 +38,70 @@ class EmailController extends Controller
         
         if($request->isMethod('POST'))
         {
-            $email_send = $request->get('email-send');
+            $email_send = $request->get('email-send', '');
             $subject    = $request->get('asunto');
             $poll_id    = $request->get('id_poll');
             
-            $token = md5($poll_id);
-            $poll = $em->getRepository('BDCPollBundle:Poll')->find($poll_id);
-            $questions = $em->getRepository('BDCPollBundle:Question')->findBy(array('id_poll' => $poll_id));
-            $answers = $em->getRepository('BDCPollBundle:Answer')->findBy(array('id_poll' => $poll_id));
+            if($email_send != ''){
+            
+                $token = md5($poll_id);
+                $poll = $em->getRepository('BDCPollBundle:Poll')->find($poll_id);
+                $questions = $em->getRepository('BDCPollBundle:Question')->findBy(array('id_poll' => $poll_id));
+                $answers = $em->getRepository('BDCPollBundle:Answer')->findBy(array('id_poll' => $poll_id));
 
-            $action =  $url = $this->generateUrl('front_vote',array(), true);
-            
-            $email_send_t = substr($email_send, 0, -1);
-            
-            $array_email = explode(';', $email_send_t);
+                $action =  $url = $this->generateUrl('front_vote',array(), true);
 
-            
-            foreach ($array_email as $k=>$email_to){
-                
-              $user = $em->getRepository('BDCPollBundle:User')->findOneByEmail($email_to);
-               
-               
-               $link = $request->getSchemeAndHttpHost().'/vote/show/'.$token.'/'.$email_to;
-               
-               $link_code = '<div>Si no puede visualizar la Encuesta <a href="'.$link.'" target="_blanck">Click Aqui</a></div><br/><br/><br/>';
-               
-               $form_code = $utils->generate_form_code($poll, $questions, $answers, $action, $user, $link_code);
-                   
-               $replacements[$email_to] = array (
-                    "{user}" => $email_to,
-                    "{body}" => $form_code
-                ); 
+                $email_send_t = substr($email_send, 0, -1);
+
+                $array_email = explode(';', $email_send_t);
+
+
+                foreach ($array_email as $k=>$email_to){
+
+                  $user = $em->getRepository('BDCPollBundle:User')->findOneByEmail($email_to);
+
+
+                   $link = $request->getSchemeAndHttpHost().'/vote/show/'.$token.'/'.$email_to;
+
+                   $link_code = '<div>Si no puede visualizar la Encuesta <a href="'.$link.'" target="_blanck">Click Aqui</a></div><br/><br/><br/>';
+
+                   $form_code = $utils->generate_form_code($poll, $questions, $answers, $action, $user, $link_code);
+
+                   $replacements[$email_to] = array (
+                        "{user}" => $email_to,
+                        "{body}" => $form_code
+                    ); 
+                }
+
+
+                // Create the mail transport configuration
+                $transport = \Swift_MailTransport::newInstance();
+
+                $plugin = new \Swift_Plugins_DecoratorPlugin($replacements);
+
+                $mailer = \Swift_Mailer::newInstance($transport);
+
+                $mailer->registerPlugin($plugin);
+
+                $message = \Swift_Message::newInstance()
+                          ->setSubject('Club Belgrano | '.$subject)
+                          ->setFrom("info@belgrano.com", 'Club Belgrano')
+                          ->setBody('{body}','text/html')  ;
+
+                // Send the email
+                foreach($array_email as $k=>$email_to) {
+                  $message->setTo($email_to);
+                  $mailer->send($message);
+                }
+
+                $sent = TRUE;
+            }else{
+                $error = 1;
             }
-            
-            
-            // Create the mail transport configuration
-            $transport = \Swift_MailTransport::newInstance();
-            
-            $plugin = new \Swift_Plugins_DecoratorPlugin($replacements);
-            
-            $mailer = \Swift_Mailer::newInstance($transport);
-            
-            $mailer->registerPlugin($plugin);
-            
-            $message = \Swift_Message::newInstance()
-                      ->setSubject('Club Belgrano | '.$subject)
-                      ->setFrom("info@belgrano.com", 'Club Belgrano')
-                      ->setBody('{body}','text/html')  ;
-            
-            // Send the email
-            foreach($array_email as $k=>$email_to) {
-              $message->setTo($email_to);
-              $mailer->send($message);
-            }
-            
-            $sent = TRUE;
             
         }
         
-        return $this->render('BDCPollBundle:Email:index.html.twig', ['polls'=>$polls, 'sent'=>$sent]);
+        return $this->render('BDCPollBundle:Email:index.html.twig', ['polls'=>$polls, 'sent'=>$sent, 'error' => $error]);
     }
 
     /**
